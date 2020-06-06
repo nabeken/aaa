2976f2f102aaf120b25db9b8b21fb29661ae3778
TANABE Ken-ichi <nabeken@tknetworks.org>
2020-06-06 21:07:22 +0900

    increment version

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 const Name string = "aaa"
-const Version string = "0.0.1"
+const Version string = "2.0.0"
 
 // GitCommit describes latest commit hash.
 // This value is extracted by git command when building.
